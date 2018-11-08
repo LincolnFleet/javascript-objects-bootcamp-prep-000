a92027285659862b90a15e4ab72f5e30fea33c94
Travis Martin <travisemerymartin@gmail.com>
2018-11-08 15:45:59 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,8 +1,8 @@
 var playlist = {'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes'}
 
-function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.artistName = songTitle
-  return playlist
+function updatePlaylist(list, artistName, songTitle) {
+  list.artistName = songTitle
+  return list
 }
 
 updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi")
