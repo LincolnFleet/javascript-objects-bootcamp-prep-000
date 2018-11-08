40bfd0a2737c4f22cd6aad7a3d54aa6efbae83f2
Travis Martin <travisemerymartin@gmail.com>
2018-11-08 15:53:26 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,3 +4,8 @@ function updatePlaylist(list, artistName, songTitle) {
   list[artistName] = songTitle
   return list
 }
+
+function removeFromPlaylist(list, artistName) {
+  delete list.artistName
+  return list
+}
